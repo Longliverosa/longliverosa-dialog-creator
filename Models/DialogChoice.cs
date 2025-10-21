c67582f95f57cc39d4c44cda6144730f42d1e32a
Lucas <lucas@muntinga.info>
2025-10-22 00:17:28 +0200

    Fix for using node in choice instead of id

--- Models/DialogChoice.cs
+++ Models/DialogChoice.cs
@@ -1,9 +1,13 @@
-﻿namespace DialogCreator.Models
+﻿using System.Text.Json.Serialization;
+
+namespace DialogCreator.Models
 {
     public class DialogChoice(string identifier)
     {
         public string Identifier { get; set; } = identifier;
+        [JsonIgnore]
         public DialogNode? DialogNode { get; set; } = null;
+        public string? DialogNodeId => DialogNode?.Identifier;
         public string Text { get; set; } = string.Empty;
     }
 }
