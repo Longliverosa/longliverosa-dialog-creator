8f228ab7c65db6e4c591da7b014910f512213db9
Lucas <lucas@muntinga.info>
2025-10-21 23:09:45 +0200

    Initialize project

--- Models/DialogNode.cs
+++ Models/DialogNode.cs
@@ -3,6 +3,7 @@
     public class DialogNode
     {
         public string Identifier { get; set; } = Utilities.Utilities.GetRandomIdentifier();
+        public string Name { get; set; } = string.Empty;
         public List<DialogText> Text { get; set; } = [];
         public List<DialogChoice> Choices { get; set; } = [];
 
